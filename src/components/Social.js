f6318e4d685cbf595e87d57666d1421a84e5e712
Amrit Thapa <amet.thapa27@gmail.com>
2021-08-08 10:59:55 +0545

    social component complete

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,30 +1,36 @@
 
- import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { FaTwitter, FaInstagram } from 'react-icons/fa';
+import {CgFacebook, CgSearch} from 'react-icons/cg';
 
-const Social = () =>{
+import React, { Components } from 'react';
+
+
+const Social= () =>{
     return (
-        <div className="flex justify-between py-7 px-10 items-center">
+        <div className="flex justify-between py-7 px-14 items-center border-b border-gray-200">
             <div className="cursor-pointer flex">
-                <div className="h-10 w-10 bg-white rounded-full flex justify-center items-center hover:bg-blue-200">
-                    <FaFacebookF className="h-5 w-5"/>
+                <div className="h-10 w-10 mr-2 bg-white rounded-full border border-gray-200 flex justify-center items-center hover:bg-indigo-400 transition ease-in duration-300 hover:text-white">
+                    <CgFacebook className="h-4 w-4"/>
                 </div>
-                <div className="h-10 w-10 bg-white rounded-full flex justify-center items-center hover:bg-blue-200">
-                    <FaTwitter className="h-5 w-5"/>
+                <div className="h-10 w-10 mr-2 bg-white rounded-full border border-gray-200 flex justify-center items-center hover:bg-indigo-400 transition ease-in duration-300 hover:text-white">
+                    <FaTwitter className="h-4 w-4"/>
                 </div>
-                <div className="h-10 w-10 bg-white rounded-full flex justify-center items-center hover:bg-blue-200">
-                    <FaInstagram className="h-5 w-5"/>
+                <div className="h-10 w-10 mr-2 bg-white rounded-full border border-gray-200 flex justify-center items-center hover:bg-indigo-400 transition ease-in duration-300 hover:text-white">
+                    <FaInstagram className="h-4 w-4"/>
                 </div>
             </div>
             <div>
-                <a href="#">
+                <a href="#" className="uppercase text-base font-bold text-xl">
                     Publishing 
-                    <span>Company</span>
-                    <small>book publishing company</small>
+                    <span className="text-red-500"> Company</span>
+                    <small className="block text-gray-500 text-xs text-center tracking-widest">book publishing company</small>
                 </a>
             </div>
-            <div>
-                <input type="text" className="border-red-700" />
-                <p>This iis shit</p>
+            <div className="flex">
+                <input type="text" className="border-black" />
+                <div className="bg-indigo-400 h-12 w-14 flex justify-center items-center text-white border rounded-r-full cursor-pointer">
+                    <CgSearch className="h-5 w-5"/>
+                </div>
             </div>
         </div>
     )
