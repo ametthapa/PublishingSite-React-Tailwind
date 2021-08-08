98271064c039e4244f7b830d0304d01832871c0c
Amrit Thapa <amet.thapa27@gmail.com>
2021-08-08 13:24:33 +0545

    showcase component

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,6 +1,16 @@
 const Showcase = () =>{
     return (
-        <div></div>
+        <div className="relative">
+            <img src="./images/3.jpg" alt="books" className="w-full h-showcase object-cover"/>
+            <div className="absolute top-48 left-32 w-2/4">
+                <p className="text-5xl font-bold tracking-wide">Good books don't give up all their secrets at once</p>
+                <p className="py-8">A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                <div className="font-bold">
+                    <button className="font-semibold bg-indigo-400 p-4 rounded-md text-white">View All Books</button>
+                    <button className="font-semibold bg-white p-4 rounded-md">Explore new</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      height : {
+        showcase: '640px',
+      },
+      color : {
+        primary : 'rgba(59, 130, 246, 0.5)',
+      }
+    }
   },
   variants: {
     extend: {},
