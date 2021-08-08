d108729f17a29189da001540ed8d40c4ecd84169
Amrit Thapa <amet.thapa27@gmail.com>
2021-08-08 11:50:12 +0545

    menu bar component

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -1,6 +1,19 @@
 const Menubar = () =>{
     return (
-        <div></div>
+        <div>
+            <nav className="tracking-widest font-semibold">
+                <ul className="flex justify-center uppercase ">
+                    <li className="px-5 py-8 text-indigo-500 border-b-2 hover:border-indigo-400 transition ease-in duration-500 opacity-50"><a href="">Home</a></li>
+                    <li className="px-5 py-8 border-b-2 hover:border-indigo-400"><a href="">About</a></li>
+                    <li className="px-5 py-8 border-b-2 hover:border-indigo-400"><a href="">coming soon</a></li>
+                    <li className="px-5 py-8 border-b-2 hover:border-indigo-400"><a href="">top seller</a></li>
+                    <li className="px-5 py-8 border-b-2 hover:border-indigo-400"><a href="">books</a></li>
+                    <li className="px-5 py-8 border-b-2 hover:border-indigo-400"><a href="">author</a></li>
+                    <li className="px-5 py-8 border-b-2 hover:border-indigo-400"><a href="">blog</a></li>
+                    <li className="px-5 py-8 border-b-2 hover:border-indigo-400"><a href="">contact</a></li>
+                </ul>
+            </nav>
+        </div>
     )
 }
 
--- src/components/Social.js
+++ src/components/Social.js
@@ -2,20 +2,19 @@
 import { FaTwitter, FaInstagram } from 'react-icons/fa';
 import {CgFacebook, CgSearch} from 'react-icons/cg';
 
-import React, { Components } from 'react';
 
 
 const Social= () =>{
     return (
         <div className="flex justify-between py-7 px-14 items-center border-b border-gray-200">
-            <div className="cursor-pointer flex">
-                <div className="h-10 w-10 mr-2 bg-white rounded-full border border-gray-200 flex justify-center items-center hover:bg-indigo-400 transition ease-in duration-300 hover:text-white">
+            <div className="flex">
+                <div className="cursor-pointer h-10 w-10 mr-2 bg-white rounded-full border border-gray-200 flex justify-center items-center hover:bg-indigo-400 transition ease-in duration-300 hover:text-white">
                     <CgFacebook className="h-4 w-4"/>
                 </div>
-                <div className="h-10 w-10 mr-2 bg-white rounded-full border border-gray-200 flex justify-center items-center hover:bg-indigo-400 transition ease-in duration-300 hover:text-white">
+                <div className="cursor-pointer h-10 w-10 mr-2 bg-white rounded-full border border-gray-200 flex justify-center items-center hover:bg-indigo-400 transition ease-in duration-300 hover:text-white">
                     <FaTwitter className="h-4 w-4"/>
                 </div>
-                <div className="h-10 w-10 mr-2 bg-white rounded-full border border-gray-200 flex justify-center items-center hover:bg-indigo-400 transition ease-in duration-300 hover:text-white">
+                <div className="cursor-pointer h-10 w-10 mr-2 bg-white rounded-full border border-gray-200 flex justify-center items-center hover:bg-indigo-400 transition ease-in duration-300 hover:text-white">
                     <FaInstagram className="h-4 w-4"/>
                 </div>
             </div>
