1f94dc9cb74d60006f195807e4fb19835d005bcc
Amrit Thapa <amet.thapa27@gmail.com>
2021-08-12 15:01:44 +0545

    menubar
    flex col type

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -7,7 +7,7 @@ const MenuItems = [{
 },{
     title:'books',
 },{
-    title:'books',
+    title:'author',
 },{
     title:'blog',
 }, {
@@ -19,7 +19,7 @@ const Menubar = () =>{
     return (
         <div>
             <nav className="tracking-widest font-semibold">
-                <ul className="flex justify-center uppercase ">
+                <ul className="flex justify-center uppercase flex-col md:flex-row">
                     <li className="px-5 py-8 text-indigo-500 border-b-2 hover:border-primary transition ease-in duration-500 opacity-50"><a href="">Home</a></li>
                     {
                         MenuItems.map((items) =>{
